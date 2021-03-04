c24bb38e47a6590951801d29a942b9a0bbe777cc
itsjustbolu <boluowolana@gmail.com>
2021-03-04 13:45:44 -0600

    update navbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 export class Topbar extends Component {
   render() {
@@ -11,14 +12,23 @@ export class Topbar extends Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Link to="/">Home</Link>
+              <Link to="/menu">Menu</Link>
+              <Link to="/place-order">Place Order</Link>
+              <Link to="/customers">Customers</Link>
+              <Link to="/payments">Payments</Link>
+              <Link to="/suppliers">Suppliers</Link>
+              <Link to="/employees">Employees</Link>
+              <Link to="/status">Status</Link>
+
+              {/* <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/menu">Menu</Nav.Link>
               <Nav.Link href="/place-order">Place Order</Nav.Link>
               <Nav.Link href="/customers">Customers</Nav.Link>
               <Nav.Link href="/payments">Payments</Nav.Link>
               <Nav.Link href="/suppliers">Suppliers</Nav.Link>
               <Nav.Link href="/employees">Employees</Nav.Link>
-              <Nav.Link href="/status">Status</Nav.Link>
+              <Nav.Link href="/status">Status</Nav.Link> */}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
